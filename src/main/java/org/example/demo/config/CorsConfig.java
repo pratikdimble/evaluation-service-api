ee1f47277ca20ef62026acf4245eaf4024b46e87
pratikdimble <pratikdimble2238@gmail.com>
2026-02-06 10:36:39 +0530

    CI-CD

---
+++ src/main/java/org/example/demo/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package org.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // or restrict to your frontend domain
+                        .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
+
--- src/main/java/org/example/demo/config/SwaggerConfig.java
+++ src/main/java/org/example/demo/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://evaluation-service-api-production.up.railway.app"))
                 .info(new Info()
                         .title("Evaluation Service API")
                         .version("v1.0")
@@ -20,10 +22,12 @@ public class SwaggerConfig {
                         .contact(new Contact()
                                 .name("Pratik Dimble")
                                 .email("pratik.dimble@nitorinfotech.com")
-                                .url("http://localhost:8080/swagger-ui/index.html"))
+//                                .url("http://localhost:8080/swagger-ui/index.html")
+                        )
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("http://springdoc.org")));
+                                .url("http://springdoc.org"))
+                );
     }
 }
 
